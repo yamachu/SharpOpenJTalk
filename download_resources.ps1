b131090cfcfc80b7f3139923c73f091f93e82091
yamachu <yamachu.dev@gmail.com>
2021-09-02 16:13:36 +0900

    Update Download script

--- download_resources.ps1
+++ download_resources.ps1
@@ -5,7 +5,14 @@ $latestVersion = $Matches.tagName
 "$latestVersion"
 $baseUrl = "https://github.com/yamachu/LibOpenJTalk/releases/download/$latestVersion"
 
-Invoke-WebRequest "$baseUrl/libopenjtalk.dylib" -OutFile library\resources\osx\libopenjtalk.dylib
-Invoke-WebRequest "$baseUrl/libopenjtalk.so" -OutFile library\resources\linux\libopenjtalk.so
-Invoke-WebRequest "$baseUrl/x86_open_jtalk.dll" -OutFile library\resources\win-x86\openjtalk.dll
-Invoke-WebRequest "$baseUrl/x64_open_jtalk.dll" -OutFile library\resources\win-x64\openjtalk.dll
+# Full
+Invoke-WebRequest "$baseUrl/libopenjtalk.dylib" -OutFile library\full\resources\osx\libopenjtalk.dylib
+Invoke-WebRequest "$baseUrl/libopenjtalk.so" -OutFile library\full\resources\linux\libopenjtalk.so
+Invoke-WebRequest "$baseUrl/x86_open_jtalk.dll" -OutFile library\full\resources\win-x86\openjtalk.dll
+Invoke-WebRequest "$baseUrl/x64_open_jtalk.dll" -OutFile library\full\resources\win-x64\openjtalk.dll
+
+# Lang
+Invoke-WebRequest "$baseUrl/libopenjtalk-lang.dylib" -OutFile library\lang\resources\osx\libopenjtalk-lang.dylib
+Invoke-WebRequest "$baseUrl/libopenjtalk-lang.so" -OutFile library\lang\resources\linux\libopenjtalk-lang.so
+Invoke-WebRequest "$baseUrl/x86_open_jtalk_lang.dll" -OutFile library\lang\resources\win-x86\openjtalk-lang.dll
+Invoke-WebRequest "$baseUrl/x64_open_jtalk_lang.dll" -OutFile library\lang\resources\win-x64\openjtalk-lang.dll
