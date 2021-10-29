0770577397e4e5d036d98073808bbd2ecb85f1a8
yamachu <yamachu.dev@gmail.com>
2021-10-29 17:34:32 +0900

    ビルド済みのライブラリをダウンロードするようにする

--- download_resources.ps1
+++ download_resources.ps1
@@ -10,9 +10,11 @@ Invoke-WebRequest "$baseUrl/libopenjtalk.dylib" -OutFile library\full\resources\
 Invoke-WebRequest "$baseUrl/libopenjtalk.so" -OutFile library\full\resources\linux\libopenjtalk.so
 Invoke-WebRequest "$baseUrl/x86_open_jtalk.dll" -OutFile library\full\resources\win-x86\openjtalk.dll
 Invoke-WebRequest "$baseUrl/x64_open_jtalk.dll" -OutFile library\full\resources\win-x64\openjtalk.dll
+Invoke-WebRequest "$baseUrl/wasm-libopenjtalk.a" -OutFile library\full\resources\browser-wasm\openjtalk.a
 
 # Lang
 Invoke-WebRequest "$baseUrl/libopenjtalk-lang.dylib" -OutFile library\lang\resources\osx\libopenjtalk-lang.dylib
 Invoke-WebRequest "$baseUrl/libopenjtalk-lang.so" -OutFile library\lang\resources\linux\libopenjtalk-lang.so
 Invoke-WebRequest "$baseUrl/x86_open_jtalk_lang.dll" -OutFile library\lang\resources\win-x86\openjtalk-lang.dll
 Invoke-WebRequest "$baseUrl/x64_open_jtalk_lang.dll" -OutFile library\lang\resources\win-x64\openjtalk-lang.dll
+Invoke-WebRequest "$baseUrl/wasm-libopenjtalk-lang.a" -OutFile library\lang\resources\browser-wasm\openjtalk-lang.a
